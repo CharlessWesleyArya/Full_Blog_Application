266370e942080a034c457503e3def950cb0a2b67
Charless Wesley Arya <wesleyarya40@gmail.com>
2022-09-11 20:26:12 +0530

    added likes functionality without limits as of now

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,6 +1,7 @@
 export default (posts = [], action) => {
   switch (action.type) {
     case "UPDATE":
+    case "LIKE":
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
       );
@@ -8,6 +9,8 @@ export default (posts = [], action) => {
       return action.payload;
     case "CREATE":
       return [...posts, action.payload];
+    case "DELETE":
+      return posts.filter((post) => post._id !== action.payload);
     default:
       return posts;
   }
